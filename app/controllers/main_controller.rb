a3e3abbe191a18861e799f0695bc4b077100ea40
Syanom <yandupliy@gmail.com>
2023-02-05 18:58:49 +0300

    create nav rough

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,4 +2,10 @@ class MainController < ApplicationController
   def home
     @slideshow_picture = SlideshowPicture.first
   end
+
+  def search
+  end
+
+  def contact_me
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'main#home'
+  get 'main/contact_me'
+  get 'main/search'
+
   namespace :admin do
     resources :slideshow_pictures
   end
