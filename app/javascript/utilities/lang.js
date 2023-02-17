a81ed2bf8e8800c980fe95a1128d710af80be6fa
Syanom <yandupliy@gmail.com>
2023-02-17 15:18:21 +0300

    add language change

---
+++ app/javascript/utilities/lang.js
@@ -0,0 +1,33 @@
+document.addEventListener('turbolinks:load', function () {
+  setCurrentLanguage()
+  document.querySelector(".current-lang").addEventListener('click', handleDropdown)
+  document.querySelectorAll(".lang-option").forEach(btn => {
+    btn.addEventListener('click', changeLang)
+  });
+})
+
+function setCurrentLanguage() {
+  let current_lang = getCurrentLang()
+  document.querySelector(".current-lang").childNodes[0].nodeValue = current_lang.charAt(0).toUpperCase() + current_lang.slice(1)
+  document.getElementById(current_lang).classList.add('hidden')
+}
+
+function handleDropdown() {
+  document.querySelector(".lang-options").classList.toggle("hidden");
+}
+
+function changeLang(e) {
+  let current_location = window.location.href.split('?')[0]
+  let query = new URLSearchParams()
+  query.set("lang", e.target.id)
+  window.location.href = current_location + '?' + query.toString()
+}
+
+function getCurrentLang() {
+  let current_lang = (new URLSearchParams(window.location.search)).get('lang')
+  if (current_lang == null) {
+    current_lang = 'en'
+  }
+
+  return current_lang
+}
