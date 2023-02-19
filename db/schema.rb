fc05a5635c2681bb761fd821cd97e93c236e067f
Syanom <yandupliy@gmail.com>
2023-02-19 21:54:02 +0300

    make migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_17_140322) do
+ActiveRecord::Schema.define(version: 2023_02_19_184631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "add_images", force: :cascade do |t|
+    t.string "image"
+    t.bigint "adds_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["adds_id"], name: "index_add_images_on_adds_id"
+  end
+
+  create_table "adds", force: :cascade do |t|
+    t.boolean "flat"
+    t.boolean "house"
+    t.boolean "for_sale"
+    t.boolean "for_rent"
+    t.integer "square"
+    t.integer "rooms"
+    t.integer "living_rooms"
+    t.integer "building_age"
+    t.integer "number_of_floors"
+    t.string "heating"
+    t.integer "number_of_bathrooms"
+    t.boolean "furnished"
+    t.string "user_status"
+    t.boolean "credit_available"
+    t.boolean "video_call_available"
+    t.boolean "active", default: true
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "contact_mes", force: :cascade do |t|
     t.string "phone_number"
@@ -31,4 +61,5 @@ ActiveRecord::Schema.define(version: 2023_02_17_140322) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "add_images", "adds", column: "adds_id"
 end
