cce7d238038f6b45f91f6db02c7149c4f849c836
Syanom <yandupliy@gmail.com>
2023-02-21 11:01:16 +0300

    create add show

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'main#home'
 
   resources :contact_mes, only: %i[new create]
-  resources :adds, only: :index
+  resources :adds, only: %i[index show]
   resources :admin, only: :index
 
   namespace :admin do
