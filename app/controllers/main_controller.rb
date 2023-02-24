e805c62f989ce7fac5cdd045554065bc58caa747
Syanom <yandupliy@gmail.com>
2023-02-24 19:05:42 +0300

    fix exception

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,6 @@ class MainController < ApplicationController
   def home
     @slideshow_pictures = SlideshowPicture.where(active: true).to_a
     @slideshow_pictures.unshift(@slideshow_pictures.pop)
-    @width = "#{@slideshow_pictures.count * 100}%"
+    @width = @slideshow_pictures.empty? ? '100%' : "#{@slideshow_pictures.count * 100}%"
   end
 end
