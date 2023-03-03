11eac7e11ebe54240fdad955abf5b03252ec4006
Syanom <yandupliy@gmail.com>
2023-03-03 13:36:47 +0400

    add add show buttons

--- app/javascript/utilities/add_show.js
+++ app/javascript/utilities/add_show.js
@@ -3,9 +3,31 @@ document.addEventListener('turbolinks:load', function () {
     document.querySelectorAll(".add-show-image").forEach(btn => {
       btn.addEventListener('click', changeMain)
     });
+    document.querySelector('.add-arrow-left').addEventListener('click', changeImage)
+    document.querySelector('.add-arrow-right').addEventListener('click', changeImage)
   }
 })
 
 function changeMain(e) {
   document.querySelector('.add-show-image-active').src = e.target.src
+  document.querySelector('.active-image').classList.remove('active-image')
+  e.target.classList.add('active-image')
+}
+
+function changeImage(e) {
+  let imageToActivate = null
+  if (e.target.classList.contains('add-arrow-right')) {
+    imageToActivate = document.querySelector('.active-image').nextSibling
+    if (imageToActivate == null) {
+      imageToActivate = document.querySelector('.active-image').parentNode.firstChild
+    }
+  } else {
+    imageToActivate = document.querySelector('.active-image').previousSibling
+    if (imageToActivate == null) {
+      imageToActivate = document.querySelector('.active-image').parentNode.lastChild
+    }
+  }
+  document.querySelector('.active-image').classList.remove('active-image')
+  document.querySelector('.add-show-image-active').src = imageToActivate.src
+  imageToActivate.classList.add('active-image')
 }
